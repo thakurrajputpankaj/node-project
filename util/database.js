a4f8bee3e77df7d23d01ac1eb0dcd1b22e7fd473
PANKAJ RAJPUT <thakurrajputpankaj0@gmail.com>
2023-09-03 01:25:10 -0400

    Update database.js

--- util/database.js
+++ util/database.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    database: 'node-complete',
-    password: '9466263943'
+    database: '',
+    password: ''
 });
 
 module.exports = pool.promise();
