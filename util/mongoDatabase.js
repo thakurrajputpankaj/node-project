6ed2744692fb97b850afbb17319c4b38bedb70d0
PANKAJ RAJPUT <thakurrajputpankaj0@gmail.com>
2023-09-03 01:24:49 -0400

    Update mongoDatabase.js

--- util/mongoDatabase.js
+++ util/mongoDatabase.js
@@ -5,7 +5,7 @@ let _db;
 const MongoClient = mongodb.MongoClient;
 
 const mongoConnect = (callback) => {
-    MongoClient.connect('mongodb+srv://thakurrajputpankaj0:9466263943@cluster0.qe7mf2g.mongodb.net/')
+    MongoClient.connect('mongodb+srv://username:password@cluster0.qe7mf2g.mongodb.net/')
         .then(client => {
             console.log('Connection Successful');
             _db = client.db();
